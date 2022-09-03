74befac3ab120142f7154ab363e5805bc29b3d6f
AlbinB97 <91016401+AlbinB97@users.noreply.github.com>
2022-09-03 19:39:05 +0200

    updated edit button / create button on schools

--- admin/src/schools.jsx
+++ admin/src/schools.jsx
@@ -1,12 +1,33 @@
-import {Datagrid, EmailField, List, TextField, UrlField} from 'react-admin';
+import { Datagrid, List, TextField, Edit, EditButton, SimpleForm, Create, TextInput} from 'react-admin';
 
 export const SchoolList = () => (
   <List>
-    <Datagrid rowClick="edit">
-      <TextField source="id" />
-      <TextField source="name" />
-      <TextField source="shortName" />
-      
-    </Datagrid>
+      <Datagrid rowClick="edit">
+          <TextField source="id" />
+          <TextField source="name" />
+          <TextField source="shortName" />
+          <EditButton />
+      </Datagrid>
   </List>
+);
+
+export const SchoolEdit = () => (
+  <Edit title="Edit schools">
+      <SimpleForm>
+          <TextInput disabled source="id" />
+          <TextInput source="name" />
+          <TextInput source="shortname" />
+      </SimpleForm>
+  </Edit>
+
+);
+
+export const SchoolCreate = props => (
+  <Create {...props}>
+      <SimpleForm>
+          <TextInput disabled source="id" />
+          <TextInput source="name" />
+          <TextInput source="shortname" />
+      </SimpleForm>
+  </Create>
 );
