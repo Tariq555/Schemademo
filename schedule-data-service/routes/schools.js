149f53be3d314802155cbabac3a4653c46bcdddf
AlbinB97 <91016401+AlbinB97@users.noreply.github.com>
2022-09-03 19:16:27 +0200

    post and put

--- schedule-data-service/routes/schools.js
+++ schedule-data-service/routes/schools.js
@@ -10,58 +10,31 @@ module.exports = function (server, db) {
     res.json(result)
   });
 
-  // // registrera en ny lärare
-  // server.post('/data/schools', (request, response) => {
-  //   let user = request.body
-  //   let encryptedPassword = encrypt(user.password)
-  //   let result
-  //   try {
-  //     result = db.prepare('INSERT INTO schools (email, password) VALUES(?,?)').run([user.email, encryptedPassword])
-  //   } catch (e) {
-  //     console.error(e)
-  //   }
-  //   response.json(result)
-  // })
+  // registrera en ny skola
+   server.post('/data/schools', (request, response) => {
+     let user = request.body
+     let encryptedPassword = encrypt(user.password)
+     let result
+     try {
+      result = db.prepare('INSERT INTO schools (email, password) VALUES(?,?)').run([user.email, encryptedPassword])
+     } catch (e) {
+       console.error(e)
+     }
+     response.json(result)
+   })
 
 
-  // // komplettera profil för användare
-  // server.put('/data/schools', (request, response) => {
-  //   let user = request.body
-  //   let result
-  //   try {
-  //     result = db.prepare('UPDATE schools SET name = ?, shortName = ?').run([user.name, user.shortName])
-  //   } catch (e) {
-  //     console.error(e)
-  //   }
-  //   response.json(result)
-  // })
-
-
-  // // begär ändring av lösenord för användare
-  // server.delete('/data/schools', (request, response) => {
-  //   let user = request.body
-  //   // let result
-  //   // try {
-  //   //   result = db.prepare('UPDATE teachers SET password = NULL WHERE email = ? AND hide = 0').run([user.email])
-  //   // } catch (e) {
-  //   //   console.error(e)
-  //   // }
-  //   response.json(result)
-  // })
-
-
-  // ändra lösenord för användare
-  // server.patch('/data/teachers/password', (request, response) => {
-  //   let user = request.body
-  //   let encryptedPassword = encrypt(user.password)
-  //   let result
-  //   try {
-  //     result = db.prepare('UPDATE teachers SET password = ? WHERE password IS NULL AND email = ?').run([encryptedPassword, user.email])
-  //   } catch (e) {
-  //     console.error(e)
-  //   }
-  //   response.json(result)
-  // })
+  // uppdatera en skola
+  server.put('/data/schools', (request, response) => {
+    let user = request.body
+    let result
+    try {
+      result = db.prepare('UPDATE schools SET name = ?, shortName = ?').run([user.name, user.shortName])
+    } catch (e) {
+      console.error(e)
+    }
+    response.json(result)
+  })
 
 
 }
