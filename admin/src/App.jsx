8edaaad91bf8e3077517aa51cce98e3828f80be9
toriqul007 <shohan.hkr@gmail.com>
2022-09-08 19:24:21 +0200

    update page and icons
    Removed password and roles from teacher update page, deleted posts and users from the admin page and Icons for the teachers, schools and classes fixed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,16 +1,12 @@
 // in src/App.js
 import * as React from "react";
 import { Admin, Resource, EditGuesser } from 'react-admin';
-import { PostList } from './posts';
-import { PostEdit } from "./post-edit";
-import { PostCreate} from "./post-create";
-import { UserList } from './users';
 import { SchoolList, SchoolEdit, SchoolCreate } from './schools';
 import { TeacherList, TeacherCreate, TeacherEdit } from './teachers';
 import { ClassesList } from './classes';
-//import jsonServerProvider from 'ra-data-json-server';
-import PostIcon from '@mui/icons-material/Book';
-import UserIcon from '@mui/icons-material/Group';
+import TeacherIcon from '@mui/icons-material/Person';
+import SchoolIcon from '@mui/icons-material/School';
+import ClassIcon from '@mui/icons-material/Class';
 import Dashboard from "./Dashboard";
 import simpleRestProvider from 'ra-data-simple-rest';
 import { getToggleButtonGroupUtilityClass } from "@mui/material";
@@ -23,11 +19,9 @@ const dataProvider = simpleRestProvider('/data');
 
 const App = () => (
   <Admin dashboard={Dashboard} dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
-    <Resource name="users" list={UserList} icon={UserIcon} />
-    <Resource name="teachers" list={TeacherList} edit={TeacherEdit} create={TeacherCreate} icon={UserIcon} />
-    <Resource name="schools" list={SchoolList} edit={SchoolEdit} create={SchoolCreate} icon={UserIcon} />
-    <Resource name="classes" list={ClassesList} icon={UserIcon} />
+    <Resource name="teachers" list={TeacherList} edit={TeacherEdit} create={TeacherCreate} icon={TeacherIcon} />
+    <Resource name="schools" list={SchoolList} edit={SchoolEdit} create={SchoolCreate} icon={SchoolIcon} />
+    <Resource name="classes" list={ClassesList} icon={ClassIcon} />
   </Admin>
 );
 
