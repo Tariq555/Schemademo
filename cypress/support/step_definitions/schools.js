f4bdd05c832dfada17a60b886dd475234730db19
AlbinB97 <91016401+AlbinB97@users.noreply.github.com>
2022-09-16 12:32:14 +0200

    again

--- cypress/support/step_definitions/schools.js
+++ cypress/support/step_definitions/schools.js
@@ -12,7 +12,7 @@ And("that I am on the admin page", () =>{
 })
 
 When("I click on the schools button", () =>{
-    cy.get('#root > div > div > div > main > MuiDrawer-root RaSidebar-docked MuiDrawer-docked css-mk2j8q > div > div > div > a').click()
+    cy.get('#root > div > div > div > main > MuiDrawer-root RaSidebar-docked MuiDrawer-docked css-mk2j8q').click()
 })
 
 Then("I should be able to create a new school", () =>{
